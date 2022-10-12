63eced986583ed85a97ad7c96cc415b3a65fdf30
DionneMaguire <dionnemaguire@yahoo.com>
2022-10-12 19:46:40 +0000

    tested profile

--- codeloco/urls.py
+++ codeloco/urls.py
@@ -20,6 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
     path('profile/', include('profiles.urls')),
-    path('', include('blog.urls'), name='blog_urls'),
     path('accounts/', include('allauth.urls')),
+    path('', include('blog.urls'), name='blog_urls'),
 ]
